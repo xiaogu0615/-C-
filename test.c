fdd5519c09a78aeb74a2dbf6652bd86e33e88afe
xiaogu0615 <83263883+xiaogu0615@users.noreply.github.com>
2023-05-11 11:33:36 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+//int main()
+//{
+//	int age = 20;//变量，int类型age是变量名
+//	//创建变量时初始化;
+//	double weight = 75.3;
+//	//可改
+//	age = age + 1;
+//	weight = weight - 10;
+//	printf("%d\n", age);
+//	printf("%f", weight);
+//
+//	return 0;
+//}
+//int a = 100;//全局变量
+//int main()
+//{
+//	int a = 10;//局部变量
+//	printf("%d", a);
+//	return 0;
+//}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum=%d\n", sum);
+	return 0;
+}
